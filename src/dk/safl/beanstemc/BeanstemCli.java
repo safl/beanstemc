81bfc21d56b59f4a638531f9a5868327b8581e9e
Simon A. F. Lund <safl@safl.dk>
2011-02-10 16:34:51 +0100

    Added software license and some notes in the README on usage...

--- src/dk/safl/beanstemc/BeanstemCli.java
+++ src/dk/safl/beanstemc/BeanstemCli.java
@@ -1,3 +1,24 @@
+/**
+ *	This file is part of Beanstemc.
+ *
+ *	Copyright (C) 2011 Simon A. F. Lund
+ *
+ *	Beanstemc is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Lesser General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Beanstemc is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public License
+ *	along with Beanstemc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Simon A. F. Lund
+ * 
+ */
 package dk.safl.beanstemc;
 
 public class BeanstemCli {
