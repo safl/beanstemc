e77a7542ce9218f32a0e37c2f9dfd016cc4655ec
Simon A. F. Lund <safl@safl.dk>
2011-02-10 16:59:05 +0100

    ...

--- src/dk/safl/beanstemc/BeanstemCli.java
+++ src/dk/safl/beanstemc/BeanstemCli.java
@@ -16,7 +16,7 @@
  *	You should have received a copy of the GNU Lesser General Public License
  *	along with Beanstemc.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @author Simon A. F. Lund
+ *	@author Simon A. F. Lund
  * 
  */
 package dk.safl.beanstemc;
